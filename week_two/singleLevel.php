bf463ba6957f826797e4475e25b6334fe60b1b49
nehapsar <138990316+nehapsar@users.noreply.github.com>
2024-02-07 15:30:20 +0530

    Single Level Inheritence

---
+++ week_two/singleLevel.php
@@ -0,0 +1,33 @@
+<?php
+
+class Employee {
+
+    private $employeeId;
+  
+    public $employeeName;
+  
+    protected $salary;
+
+    public function __construct($employeeId, $employeeName, $salary) {
+        $this->employeeId = $employeeId;
+        $this->employeeName = $employeeName;
+        $this->salary = $salary;
+    }
+
+}
+
+class TotalSalaryOfEmployee extends Employee {
+    
+
+    public function calculateBonus($bonusAmount) {
+
+        return $this->salary * $bonusAmount;
+
+    }
+}
+
+$employeeOne = new TotalSalaryOfEmployee("102", "john", 2500);
+
+echo "Total Salary = ".$employeeOne->calculateBonus(2);
+
+?>
