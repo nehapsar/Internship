2a1a5e39e61ca54bbc424f1bd48672acdcc7c8c1
nehapsar <138990316+nehapsar@users.noreply.github.com>
2024-02-07 17:50:53 +0530

    Update Class_example.php

--- week_two/Class_example.php
+++ week_two/Class_example.php
@@ -3,34 +3,29 @@
 class Tree {
 
     public $name;
-    public $type_of_fertilizer_used;
-    public $height_of_tree;
+    public $typeOfFertilizerUsed; 
+    public $heightOfTree;
 
-    function __construct($name, $type_of_fertilizer_used, $height_of_tree) 
-    {
+    function __construct($name, $typeOfFertilizerUsed, $heightOfTree){
         $this->name = $name;
-        $this->type_of_fertilizer_used = $type_of_fertilizer_used;
-        $this->height_of_tree = $height_of_tree;
+        $this->typeOfFertilizerUsed = $typeOfFertilizerUsed; 
+        $this->heightOfTree = $heightOfTree;
     }
 
-    function get_name()
-    {
+    function getName(){
         return $this->name;
     }
 
-    function get_type_of_fertilizer_used() 
-    {
-        return $this->type_of_fertilizer_used;
+    function getTypeOfFertilizerUsed(){
+        return $this->typeOfFertilizerUsed;
     }
 
-    function get_height_of_tree()
-    {
-        return $this->height_of_tree;
+    function getHeightOfTree(){
+        return $this->heightOfTree;
     }
 }
 
 $Garden = [];
-
 
 $treesData = [
     ["Apple Tree", "NPK", "22m"],
@@ -39,21 +34,16 @@ $treesData = [
     ["Persimon Tree", "NPK", "220m"]
 ];
 
-
-
-for ($i = 0; $i < count($treesData); $i++) 
-{
+for ($i = 0; $i < count($treesData); $i++) {
     $treeData = $treesData[$i];
     $tree = new Tree($treeData[0], $treeData[1], $treeData[2]);
     $Garden[$i] = $tree;
 }
 
-
-foreach ($Garden as $tree) 
-{
-    echo $tree->get_name() . "\n";
-    echo $tree->get_type_of_fertilizer_used() . "\n";
-    echo $tree->get_height_of_tree() . "\n";
+foreach ($Garden as $tree){
+    echo $tree->getName() . "\n";
+    echo $tree->getTypeOfFertilizerUsed() . "\n"; 
+    echo $tree->getHeightOfTree() . "\n"; 
 }
 
 print_r($Garden);
