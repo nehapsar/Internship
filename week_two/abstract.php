681693a7e949f24cc621fc5a9ba987f933470909
nehapsar <138990316+nehapsar@users.noreply.github.com>
2024-02-08 22:34:01 +0530

    Example for absract class

--- week_two/abstract.php
+++ week_two/abstract.php
@@ -2,9 +2,7 @@
 
 /*Write the following code in your editor below:
     1. A class named Arithmetic with a method named add that takes  integers as parameters and returns an integer denoting their sum.
-    2. A class named Adder that inherits from a parent class named Arithmetic.
-       Your classes should not be be public
-       */
+    2. A class named Adder that inherits from a parent class named Arithmetic.*/
 
 
      abstract class Arithmetic {
