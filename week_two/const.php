23a81458bd610f9f0c793454aabf936d83c19720
nehapsar <138990316+nehapsar@users.noreply.github.com>
2024-02-09 20:55:24 +0530

    example of constants

--- week_two/const.php
+++ week_two/const.php
@@ -1,7 +1,8 @@
 <?php
-class Reptiles {
-    const scientificFamily = "reptilia";
 
+class Reptiles {
+    
+    const scientificFamily = "reptilia";
     const domain = "Eukaryota";
     const kingdom = "Animalia";
     const phylum="chordata";
@@ -9,23 +10,17 @@ class Reptiles {
     public $weightOfSpecies;
 
     public function __construct($nameOfSpecies,$weightOfSpecies){
-
        $this->nameOfSpecies=$nameOfSpecies;
        $this->weightOfSpecies = $weightOfSpecies;
        $this->scientificFamily = self::scientificFamily;
        $this->domain = self::domain;
        $this->kingdom = self::kingdom;
        $this->phylum= self::phylum;
-
    }
-
 }
 
-
 $lizard = new Reptiles("Lizard","20g");
-
 $snake = new Reptiles("Cobra","3kg");
-
 
 echo "Name of Species   : ".$lizard->nameOfSpecies."\n";
 echo "Weight of Species : ".$lizard->weightOfSpecies."\n";
@@ -34,8 +29,6 @@ echo "Domain of Species : ".$lizard->domain."\n";
 echo "Kingdom of Species: ".$lizard->kingdom."\n";
 echo "Phylum of Species : ".$lizard->phylum."\n";
 
-echo "\n";
-
 echo "Name of Species   : ".$snake->nameOfSpecies."\n";
 echo "Weight of Species : ".$snake->weightOfSpecies."\n";
 echo "Scentific Family  : ".$snake->scientificFamily."\n";
@@ -43,7 +36,7 @@ echo "Domain of Species : ".$snake->domain."\n";
 echo "Kingdom of Species: ".$snake->kingdom."\n";
 echo "Phylum of Species : ".$snake->phylum."\n";
 
-
+?>
 
 
 
