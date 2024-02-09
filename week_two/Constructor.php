f13dbea86c515afab1728813917978066836ddcd
nehapsar <138990316+nehapsar@users.noreply.github.com>
2024-02-09 20:40:58 +0530

    constructor example

--- week_two/Constructor.php
+++ week_two/Constructor.php
@@ -1,27 +1,22 @@
 <?php
 
-class Student {
-    
+class Student {    
     public $name;
     public $age;
     public $usn;
     const section = "A section";
-
-    public function __construct($name, $age, $usn) 
-    {
+    public function __construct($name, $age, $usn){
         $this->name = $name;
         $this->age = $age;
         $this->usn = $usn;
         $this->section =self::section;
     }
-    
 }
 
 $i = 0;
 $studentOne = new Student("Ravi", 22, "1RVMC10".++$i);
 $studentTwo = new Student("Raju", 24, "1RVMC10".++$i);
 $studentThree = new Student("Sham", 20, "1RVMC10".++$i);
-
 
 echo "NAME : ".$studentOne->name."\n";
 echo "AGE : ".$studentOne->age."\n";
