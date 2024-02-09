6ca7e17be10a5e1f71398a56d1c19630fc6b970f
nehapsar <138990316+nehapsar@users.noreply.github.com>
2024-02-09 20:49:50 +0530

    setters and getters example

--- week_two/Setter_Getter.php
+++ week_two/Setter_Getter.php
@@ -4,60 +4,41 @@
    
        public $color;
        public $NoOfSeats;
-
        public $engineCapacity;
-
        public $price;
 
        public function setColor($color){
         $this->color = $color;
        }
-
        public function getColor(){
         return $this->color;
        }
-
-
-
        public function setNoOfSeats($NoOfSeats){
         $this->NoOfSeats = $NoOfSeats;
        }
        public function getNoOfSeats(){
         return $this->NoOfSeats;
        }
-
-
-
-        public function setEngineCapacity($engineCapacity){
+       public function setEngineCapacity($engineCapacity){
             $this->engineCapacity=$engineCapacity;
         }
-
         public function getEngineCapacity(){
             return $this->engineCapacity;
         }
-
-
-
         public function setPrice($price){
             $this->price =$price;
         }
-
         public function getPrice(){
             return $this->price;
         }
-
     }
 
-    
-
         $volvo = new Car();
-
         $volvo->setcolor("Black");
         $volvo->setNoOfSeats("7");
         $volvo->setEngineCapacity("50000cc");
         $volvo->setPrice("700000");
-
-         
+    
         echo "Name  Volvo Cras\n";
         echo "My features are:\n";
         echo "Color : ".$volvo->getColor()."\n";
@@ -65,7 +46,7 @@
         echo "Engine Capacity : ".$volvo->getEngineCapacity()."\n";
         echo "Price : ".$volvo->getPrice();
 
-
+?>
 
 
 
