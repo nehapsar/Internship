aa2eb0e18a6e2aee7e0891e5a4c89d27447e0fd1
nehapsar <138990316+nehapsar@users.noreply.github.com>
2024-02-09 21:00:52 +0530

    example for multi level example

--- week_two/multilevel_inheritance.php
+++ week_two/multilevel_inheritance.php
@@ -12,29 +12,20 @@ class Vehicle {
         $this->color = $color;
     }
 }
-
-
 //class car extending the properties from vehicle
-
 class Car extends Vehicle {
-
   
   public $engineNumber;
   public $engineCapacity;
   public $mpg;
-  
 
   public function __construct($noOfWeehls, $price, $color, $engineNumber, $engineCapacity, $mpg) {
-    
         parent::__construct($noOfWeehls, $price, $color);
         $this->engineNumber = $engineNumber;
         $this->engineCapacity = $engineCapacity;
         $this->mpg = $mpg;
-
-    
     }
 }
-
 
 class Bmw extends Car {
   
@@ -42,30 +33,23 @@ class Bmw extends Car {
     public $electronicBrakeDistribution;
     public $numberOfSeats;
 
-
     public function __construct($noOfWeehls, $price, $color, $engineNumber, $engineCapacity, $mpg,$horsePower, $electronicBrakeDistribution, $numberOfSeats) {
             parent::__construct($noOfWeehls, $price, $color, $engineNumber, $engineCapacity, $mpg);
             $this->horsePower=$horsePower;
             $this->electronicBrakeDistribution=$electronicBrakeDistribution;
             $this->numberOfSeats=$numberOfSeats;
-            
-        }
-    
-  }
-
-
+    }
+}
 
  $bmwOne = new Bmw(4, 50000, 'Black', '723', '2000cc', 25,'444hp','yes availabe',7);
-
-
-
+ 
  echo "Number Of Wheels : ".$bmwOne->noOfWeehls . "\n";
  echo "Price : ".$bmwOne->price."\n";
  echo "Color".$bmwOne->color."\n";
  echo "Engine Number : ".$bmwOne->engineNumber."\n";
  echo "Engine Capacity : ".$bmwOne->engineCapacity."\n";
  echo "Mpg : ".$bmwOne->mpg."\n";
-  echo "Horse Power : ".$bmwOne->horsePower."\n";
+ echo "Horse Power : ".$bmwOne->horsePower."\n";
  echo "Electronic Break Distribution : ".$bmwOne->electronicBrakeDistribution."\n";
  echo "Number Of Seats : ".$bmwOne->numberOfSeats."\n";
 
