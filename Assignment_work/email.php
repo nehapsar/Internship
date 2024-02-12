cc47dca9f01cb6780aed170383876407a8daa3b6
nehapsar <138990316+nehapsar@users.noreply.github.com>
2024-02-12 07:58:27 +0530

    Email application

--- Assignment_work/email.php
+++ Assignment_work/email.php
@@ -1,7 +1,7 @@
 <?php
 
 /*Scenario: You are an IT Support Administrator Specialist and are charged with the task of creating email accounts for new hires.
-Generate an email with the following syntax: firstname. lastname.@department.company
+Generate an email with the following syntax: firstname. lastname.department@companyname
 Determine the department (sales, development, accounting), if none leave blank
 Generate a random String for a password
 Have set methods to change the password, set the mailbox capacity, and define an alternate email address
