d9fe3828be0b1039634723c8f4bfcd667585eeda
nehapsar <138990316+nehapsar@users.noreply.github.com>
2024-02-12 20:33:44 +0530

    Email application

--- Assignment_work/email.php
+++ Assignment_work/email.php
@@ -6,7 +6,7 @@ Determine the department (sales, development, accounting), if none leave blank
 Generate a random String for a password
 Have set methods to change the password, set the mailbox capacity, and define an alternate email address
 Have get methods to display the name, email, and mailbox capacity*/
-                                                                                                                                                                                                                                                                                                                >
+         
 class User {
     
     protected $firstName;
