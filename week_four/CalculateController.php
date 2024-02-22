7ceb69c392e978bd5b9616c544489cb48cf595e6
nehapsar <138990316+nehapsar@users.noreply.github.com>
2024-02-22 10:41:56 +0530

    Update CalculateController.php

--- week_four/CalculateController.php
+++ week_four/CalculateController.php
@@ -4,44 +4,94 @@ namespace App\controllers;
 
 class CalculateController{
 
-    public function add($numOne,$numTwo){
-           if(!is_int($numOne) || !is_int($numTwo)){
-           return ["error" => "Invalid Input"];
-           }
+    public $addition = [] ;
+    public $subtraction =[];
+    public $multplication =[];
+    public $division=[];
+    public $reminder=[];
 
-       return $numTwo+$numOne;
+    public function add($numOne,$numTwo){
+        if(!is_int($numOne) || !is_int($numTwo)){
+            return ["error" => "Invalid Input"];
+        }
+
+        $result = $numOne + $numTwo;
+        $data = ['operation' => 'add','numOne' => $numOne,'numTwo' => $numTwo,'result' => $result,];
+        $jsonFile = "arithmeticOperation.json";
+        $currentData = file_get_contents($jsonFile);
+        $currentArray = json_decode($currentData, true);
+        $currentArray['addition'][] = $data;
+        $jsonData = json_encode($currentArray);
+        file_put_contents($jsonFile, $jsonData);
+
+        return $result;
     }
-  
+
     public function subtract($numOne,$numTwo){
         if(!is_int($numOne) || !is_int($numTwo)) {
             return ["error" => "Invalid Input"];
         }
+        
+        $result = $numOne - $numTwo;
+        $data = ['operation' => 'sub','numOne' => $numOne,'numTwo' => $numTwo,'result' => $result,];
+        $jsonFile = "arithmeticOperation.json";
+        $currentData = file_get_contents($jsonFile);
+        $currentArray = json_decode($currentData, true);
+        $currentArray['subtraction'][] = $data;
+        $jsonData = json_encode($currentArray);
+        file_put_contents($jsonFile, $jsonData);
 
-       return $numTwo - $numOne;
+        return $result;
     }
-  
+    
     public function multiplication($numOne,$numTwo){
-        if(!is_int($numOne) || !is_int($numTwo)){
+        if (!is_int($numOne) || !is_int($numTwo)) {
             return ["error" => "Invalid Input"];
         }
-      
-        return $numOne * $numTwo;
+        
+        $result = $numOne * $numTwo;
+        $data = ['operation' => 'multiply', 'numOne' => $numOne, 'numTwo' => $numTwo, 'result' => $result,];
+        $jsonFile = "arithmeticOperation.json";
+        $currentData = file_get_contents($jsonFile);
+        $currentArray = json_decode($currentData, true);
+        $currentArray['multplication'][] = $data;
+        $jsonData = json_encode($currentArray);
+        file_put_contents($jsonFile, $jsonData);
+        
+        return $result;
     }
-  
+    
     public function division($numOne,$numTwo){
         if(!is_int($numOne) || !is_int($numTwo) || $numTwo == 0 || ($numTwo == 0 && $numOne =0)){
             return ["error" => "Invalid Input"];
         }
-  
-      return $numOne / $numTwo;
+        
+        $result = $numOne/$numTwo;
+        $data =["operation" =>"divsion","numOne"=>$numOne,"numTwo" =>$numTwo,"result" =>$result];
+        $jsonFile="arithmeticOperation.json";
+        $currentData = file_get_contents($jsonFile);
+        $currentArray = json_decode($currentData ,true);
+        $currentArray['division'][] = $data;
+        $jsonData = json_encode($currentArray);
+        file_put_contents($jsonFile,$jsonData);
+        
+        return $result;
     }
-
-  public function reminder($numOne,$numTwo){
+    
+    public function reminder($numOne,$numTwo){
         if(!is_int($numOne) || !is_int($numTwo) || $numTwo == 0 || ($numTwo == 0 && $numOne =0)){
             return ["error" => "Invalid Input"];
         }
-
-        return $numOne % $numTwo;
-    }
-  
+        
+        $result = $numOne%$numTwo;
+        $data =["operation" =>"divsion","numOne"=>$numOne,"numTwo" =>$numTwo,"result" =>$result];
+        $jsonFile="arithmeticOperation.json";
+        $currentData = file_get_contents($jsonFile);
+        $currentArray = json_decode($currentData ,true);
+        $currentArray['reminder'][] = $data;
+        $jsonData = json_encode($currentArray);
+        file_put_contents($jsonFile,$jsonData);
+        
+        return $result;
+   }
 }
